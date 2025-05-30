9aa571c52b5f8f1815aa67648b1ded1e989f9fba
GaGionCay <minhtrihoangngoc@gmail.com>
2025-05-30 11:26:59 +0700

    bin for git ignore

--- exe201/obj/Debug/net8.0/exe201.AssemblyInfo.cs
+++ exe201/obj/Debug/net8.0/exe201.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exe201")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0984b103bc622713cfbc6b8fd3b416e536f14fa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a065925b945ab92ce9c0c37d3d28413848d6cacd")]
 [assembly: System.Reflection.AssemblyProductAttribute("exe201")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exe201")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
